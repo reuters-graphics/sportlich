fae7ce0c11496de2d8be688c79a748b44c80eba8
freedmand <freedmand@gmail.com>
2021-05-27 11:16:52 -0400

    Slough of new routes

--- src/apis/soccer.js
+++ src/apis/soccer.js
@@ -14,4 +14,50 @@ export class Soccer extends Sportlich {
   async match(fixtureUuid) {
     return await this.getUrl(`/soccerdata/match/<auth>/${fixtureUuid}/`);
   }
+
+  async matchStats(fixtureUuid) {
+    return await this.getUrl(`/soccerdata/matchstats/<auth>/${fixtureUuid}/`);
+  }
+
+  async matchEvents(fixtureUuid) {
+    return await this.getUrl(`/soccerdata/matchevent/<auth>/${fixtureUuid}/`);
+  }
+
+  async passMatrix(fixtureUuid) {
+    return await this.getUrl(`/soccerdata/passmatrix/<auth>/${fixtureUuid}/`);
+  }
+
+  async possession(fixtureUuid) {
+    return await this.getUrl(`/soccerdata/possession/<auth>/${fixtureUuid}/`);
+  }
+
+  async commentary(fixtureUuid) {
+    return await this.getUrl(`/soccerdata/commentary/<auth>/${fixtureUuid}/`);
+  }
+
+  async matchPreview(fixtureUuid) {
+    return await this.getUrl(`/soccerdata/matchpreview/<auth>/${fixtureUuid}/`);
+  }
+
+  async matchFacts(fixtureUuid) {
+    return await this.getUrl(`/soccerdata/matchfacts/<auth>/${fixtureUuid}/`);
+  }
+
+  async possessionEvent(fixtureUuid) {
+    return await this.getUrl(
+      `/soccerdata/possessionevent/<auth>/${fixtureUuid}/`
+    );
+  }
+
+  async matchFactsBetting(fixtureUuid) {
+    return await this.getUrl(
+      `/soccerdata/matchfactsbetting/<auth>/${fixtureUuid}/`
+    );
+  }
+
+  async matchFactsAll(fixtureUuid) {
+    return await this.getUrl(
+      `/soccerdata/matchfactsall/<auth>/${fixtureUuid}/`
+    );
+  }
 }
--- src/clis/soccer.js
+++ src/clis/soccer.js
@@ -17,4 +17,55 @@ export default commandAdapter("soccer", Soccer, [
     "Get a fixture or fixture list with match details, such as date, start time, contestants, competition, season, score, result and lineups.",
     (soccer) => soccer.match,
   ],
+  [
+    "matchstats <fixtureUuid>",
+    "Get detailed match statistics for teams and each individual player, including passes, shots, crosses, tackles and more.",
+    (soccer) => soccer.matchStats,
+  ],
+  [
+    "commentary <fixtureUuid>",
+    "Get an automated and/or a manual set of commentary messages to update the reader on the key match events.",
+    (soccer) => soccer.commentary,
+  ],
+  [
+    "matchpreview <fixtureUuid>",
+    "Get match preview data, information about previous meetings between contestants and each contestant's form in the last six matches",
+    (soccer) => soccer.matchPreview,
+  ],
+  [
+    "matchfactsbetting <fixtureUuid>",
+    "Get automated betting match facts for a specific game.",
+    (soccer) => soccer.matchFactsBetting,
+  ],
+
+  [
+    "matchevents <fixtureUuid>",
+    "[ACCESS NEEDED] Get all events in a game - including the player, team, event, type, time (minute and second) - and qualifiers for each action.",
+    (soccer) => soccer.matchEvents,
+  ],
+  [
+    "passmatrix <fixtureUuid>",
+    "[ACCESS NEEDED]  Get the number of completed passes of all player combinations and x/y coordinates of their average pitch positions during the match.",
+    (soccer) => soccer.passMatrix,
+  ],
+  [
+    "possession <fixtureUuid>",
+    "[ACCESS NEEDED] Get a breakdown of ball possession during a match, including overall % possession and territorial advantage, split by time period.",
+    (soccer) => soccer.possession,
+  ],
+  [
+    "matchfacts <fixtureUuid>",
+    "[ACCESS NEEDED] Get match facts - interesting information and comments related to the match - as written by our Data Editorial team.",
+    (soccer) => soccer.matchFacts,
+  ],
+  [
+    "possessionevent <fixtureUuid>",
+    "[ACCESS NEEDED] Get all possession data for each action in a game, including player, team, event, type, minute, second, and qualifiers.",
+    (soccer) => soccer.possessionEvent,
+  ],
+  [
+    "matchfactsall <fixtureUuid>",
+    "[ACCESS NEEDED] Get all match facts - interesting information and comments related to the match.",
+    (soccer) => soccer.matchFactsAll,
+  ],
 ]);
